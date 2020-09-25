f4cf98ceb6a53e91595761f15e98d1fda4d44ce1
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-24 22:35:05 -0700

    Added data container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import BackgroundParticles from './components/BackgroundParticles/BackgroundParticles';
-import MovingCircle from './components/MovingCircle/MovingCircle';
 import Navbar from './components/Navbar/Navbar';
 import Search from './components/Search/Search';
+import AirDataContainer from './containers/AirDataContainer/AirDataContainer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <BackgroundParticles /> */}
       <Search />
-      <MovingCircle />
+      <AirDataContainer />
     </div>
   );
 }
