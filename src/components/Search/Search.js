c5ddb31d683bee95bdad2f6fc984fa4a3742adea
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-25 22:23:47 -0700

    Added debounce for input (did not handle emty string)

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import './Search.css'
+import './Search.css';
+import debounce from 'lodash/debounce';
 
 class Search extends React.Component {
   state = {
-    selected: '',
+    inputField: '',
   }
+
+  handleSearch = (val) => {
+    console.log(val)
+    // this.props.handleSearchEnter
+  }
+
+  search = debounce((text) => {
+    console.log(text)
+  }, 400);
+
 
   render() {
     return (
       <div id="search-box">
         <form>
-          <label for="city-input">Enter City or ZIP Code:</label>
-          <input type="text" id="city-input" />
+          <label htmlFor="city-input">Enter City or ZIP Code:</label>
+          <input type="text" id="city-input" onChange={(e) => this.search(e.target.value)}/>
         </form>
       </div>
     )
