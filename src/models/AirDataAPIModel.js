f0ee31cc4d6cb4055023712893a904edf87d4ec8
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-25 22:24:12 -0700

    Added a model for api calls

--- src/models/AirDataAPIModel.js
+++ src/models/AirDataAPIModel.js
@@ -1,6 +1,6 @@
 class AirDataAPIModel {
   static getDataByZipCode = (zipCode) => {
-    fetch(`https://air-quality-data-app.herokuapp.com/api/v1/get_by_zip_code/${zip_code}`).then(res => res.json());
+    fetch(`https://air-quality-data-app.herokuapp.com/api/v1/get_by_zip_code/${zipCode}`).then(res => res.json());
   }
 
   static getDataByCityName = (cityName) => {
