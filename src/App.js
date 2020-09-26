7da52f524ff21c18cd4f19786c23baa2d8958daf
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-25 22:24:33 -0700

    Changed to class component and added a skeleton of a search funtion

--- src/App.js
+++ src/App.js
@@ -3,24 +3,34 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Search from './components/Search/Search';
 import AirDataContainer from './containers/AirDataContainer/AirDataContainer';
+import AirDataAPIModel from './models/AirDataAPIModel';
 
 class App extends React.Component {
   state = {
-    selectedCity: ''
+    selectedCity: '',
+    PM: '',
+    ozone: '',
   };
 
   componentDidMount() {
-    //store {starredZipCode: 12345}
-    let zipCode = localStorage.getItem('starredZipCode');
-    if (zipCode) {
-
-    }
+    // //store {starredZipCode: 12345}
+    // let zipCode = localStorage.getItem('starredZipCode');
+    // if (undefined !== zipCode && zipCode !== '') {
+    //   AirDataAPIModel.getDataByZipCode(zipCode).then(res => {
+    //     console.log(res)
+    //   })
+    // }
   }
+
+  handleSearchEnter = (val) => {
+    console.log(val);
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Search />
+        <Search handleSearchEnter={this.handleSearchEnter}/>
         <AirDataContainer />
       </div>
     );
