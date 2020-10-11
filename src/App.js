8c9b5a690e77d5172de19345b3560b7dc83b5de3
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-10-10 17:16:53 -0700

    Added API call for getting data

--- src/App.js
+++ src/App.js
@@ -14,18 +14,39 @@ class App extends React.Component {
 
   componentDidMount() {
     // Will grab zipcode from localStorage if user saved a city and load data about this city when user opens an app.
-  }
+  };
 
   handleSearchEnter = (val) => {
-    // console.log(val);
-  }
+    AirDataAPIModel.getDataByZipCode(val)
+      .then(res => {
+        if (res.status === 200 && res.record.length > 0) {
+          let city = res.record[0].ReportingArea;
+          let ozone = res.record[0].AQI;
+          let PM = res.record[1].AQI;
+          this.setState({
+            selectedCity: city,
+            PM: PM,
+            ozone: ozone,
+          });
+        } else {
+          this.setState ({
+            selectedCity: 'No data for this area'
+          })
+        }
+      }).catch(err => { console.log(err) })
+  };
 
   render() {
+    let data = {};
+    if (this.state.selectedCity) {
+      data = this.state;
+    }
+
     return (
       <div className="App">
         <Navbar />
         <Search handleSearchEnter={this.handleSearchEnter}/>
-        <AirDataContainer />
+        <AirDataContainer data={data}/>
       </div>
     );
   }
